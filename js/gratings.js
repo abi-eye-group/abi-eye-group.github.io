5c0660250b9d9df618a42b1e890ed8cde91b95ec
jtur044 <j.turuwhenua@auckland.ac.nz>
2023-12-22 12:48:49 +1300

    updated for resting parameters

--- js/gratings.js
+++ js/gratings.js
@@ -27,39 +27,44 @@ STARTUP PARAMETERS FOR DAT.GUI CONTROLLER
 
 let parameters, last_parameters;
 
-
 function defaultParameters () {
 
-	let parameters = {  logMAR        : 1.0,
-											brightness 		: 1.0,					
-											speed 				: 2.5,						
-											direction 		: "left", 																				
-											stimulus_type : "disks", 
+
+	parameters = {  logMAR        : 1.0,
+										brightness 		: 1.0,					
+										speed 				: 2.5,						
+										direction 		: "left", 																				
+										stimulus_type : "disks", 
 
 
-						ratio: { 	"1:2"   : 2.0, 
-								 			"1:1.5" : 1.5 },
+					ratio: { 	"1:2"   : 2.0, 
+							 			"1:1.5" : 1.5 },
 
-						bars: { 			frequency 		: 1.0,
-													contrast  		: 1.0 }, 
-						sinusoids: { 	frequency 		: 1.0,
-													contrast  		: 1.0 }, 
-						disks: {	ratio    							: "1:2",
-											central_intensity  		: 0.75, 
-											surround_intensity 		: 0.45,
-											background_intensity 	: 0.5,				
-											field_spacing 				: 2.0 },
-						 sweep: {  enable								: true,
-						 					 step_duration				: 0.5,
-						 					 step_size						: 0.1,					 					 
-						 					 logMAR               : 1.0,		// internal logMAR
-						 					 direction						: -1			// internal direction 
-						 				 }								 
-						};
+					bars: { 			frequency 		: 1.0,
+												contrast  		: 1.0 }, 
+					sinusoids: { 	frequency 		: 1.0,
+												contrast  		: 1.0 }, 
+					disks: {	ratio    							: "1:2",
+										central_intensity  		: 0.75, 
+										surround_intensity 		: 0.45,
+										background_intensity 	: 0.5,				
+										field_spacing 				: 2.0 },
+					 sweep: {  enable								: true,
+					 					 step_duration				: 0.5,
+					 					 step_size						: 0.1,					 					 
+					 					 logMAR               : 1.0,		// internal logMAR
+					 					 direction						: -1			// internal direction 
+					 				 }								 
+					};
+
 
 	last_parameters = JSON.parse(JSON.stringify(parameters));
 
 };
+
+defaultParameters ();
+
+
 
 
 /* ------------------------------------------------------------------------------
